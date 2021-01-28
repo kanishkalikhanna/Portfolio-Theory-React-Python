74c4ab12bb022c91ead2681bca32ba3d185d02f6
Kanishk Ali Khanna <kanishkalikhanna@8afbfd65.st-andrews.ac.uk>
2021-01-28 21:42:27 +0000

    tested project on new mac

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request
 from optimal_portfolio_calculator import OptimalPortfolioCalculator
 from flask_cors import CORS
-from json import dumps
 
 app = Flask(__name__)
 CORS(app)
@@ -13,3 +12,7 @@ def optimal_portfolio():
     optimal_portfolio_calculator = OptimalPortfolioCalculator(request)
     result = optimal_portfolio_calculator.find_optimal_portfolio()
     return result
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
